f761e8a8bdfc19edb6b709015df6245e29459cd3
oktawn <belyhmitia@gmail.com>
2024-04-11 16:14:06 +0500

    add func on DesButton (need optimization)

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,19 +1,18 @@
 import './App.css';
 import Card from './components/Cards';
-import ButtonMy from './components/Button';
+import ButtonMy, { ButtonDes } from './components/Button';
 import { useState } from 'react';
 
 
 function App() {
   const [card, setCard] = useState();
 
-  const onUpdate = (newCard) => {
+  const onUpdateCompetences = (newCard) => {
     setCard(newCard)
   }
   const onStart = () => {
     setCard()
   }
-
 
   return (
     <div className="App">
@@ -22,9 +21,9 @@ function App() {
         <div className='card-footer'>
           <p><ButtonMy name={"My card"} onUpdate={onStart} /></p>
 
-          <p><a>My competencies </a> <ButtonMy name={"start"} onUpdate={onUpdate} /> <ButtonMy name={"next"} onUpdate={onUpdate} /> </p>
+          <p><a>My competencies </a> <ButtonMy name={"start"} onUpdate={onUpdateCompetences} /> <ButtonMy name={"next"} onUpdate={onUpdateCompetences} /> </p>
 
-          <p><a>Desired competencies </a> <ButtonMy name={"start"} /> <ButtonMy name={"next"} /></p>
+          <p><a>Desired competencies </a> <ButtonDes name={"start"} onUpdate={onUpdateCompetences} /> <ButtonDes name={"next"} onUpdate={onUpdateCompetences} /></p>
         </div>
       </body>
     </div>
--- homework/src/components/Button.jsx
+++ homework/src/components/Button.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 
+
+
+
 function ButtonMy({ name, onUpdate }) {
     const [pos, setPos] = useState(0);
     function handleClick() {
-        if (name == 'next') {
+        if (name == "next") {
             if (pos >= my_comp.length - 1) {
                 setPos(0);
             } else {
@@ -16,20 +19,45 @@ function ButtonMy({ name, onUpdate }) {
                 setPos(0);
             }
         }
-
         onUpdate({
             img_src: my_comp[pos][0],
             text: my_comp[pos][1],
         });
     }
-
     return (
         <button onClick={handleClick}>{name}</button>
     );
 };
 
+export function ButtonDes({ name, onUpdate }) {
+    const [pos, setPos] = useState(0);
+    function handleClick() {
+        if (name == "next") {
+            if (pos >= des_comp.length - 1) {
+                setPos(0);
+            } else {
+                setPos(pos + 1);
+            }
+        } else {
+            if (pos > 0) {
+                setPos(pos - 1);
+            } else {
+                setPos(0);
+            }
+        }
+        onUpdate({
+            text: des_comp[pos],
+        });
+    }
+    return (
+        <button onClick={handleClick}>{name}</button>
+    );
+}
+
+
 
 const my_comp = [["./images/happy.jpg", "главная компитенция"], ["./images/sleep.jpg", "любимое дело"], ["./images/programming.jpg", "что обычно делаю"], ["./images/c++.jpg ", "любимый язык"]];
 
+const des_comp = ["научиться строить полноценные приложения", "изучить основы верски", "понять связь между front/back", "работать с БД", "научиться работать с контейнеризацией", "постичь принцип CI/CD", "на этом поток желании пока заканчивается ＼（〇_ｏ）／"]
 
 export default ButtonMy;
